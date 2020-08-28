21e2b2a47117d4ef7c14f5e0db2e21667d26a6d9
Education <Tester900@gmail.com>
2020-08-29 01:10:04 +0300

    Sample code

--- js/index.js
+++ js/index.js
@@ -10,9 +10,50 @@ const para = document.createElement("p");
 sect.appendChild(para);
 para.textContent = "We hope you enjoyed the ride.";
 
-//створюємо 10 параграфів
-for (let i = 1; i <= 1000; i++) {
-  const p = document.createElement("p");
-  p.textContent = i + " lorem ipsum";
-  sect.appendChild(p);
+// //створюємо 10 параграфів
+// for (let i = 1; i <= 1000; i++) {
+//   const p = document.createElement("p");
+//   p.textContent = i + " lorem ipsum";
+//   sect.appendChild(p);
+// }
+
+// //Модний підхід
+// [...Array(1000)].forEach((_, i) => {
+//   const p = document.createElement("p");
+//   p.textContent = i + " lorem ipsum";
+//   sect.appendChild(p);
+// });
+
+//зімнюємо розміщення параграфа
+const linkPara = document.querySelector("p");
+sect.appendChild(linkPara.cloneNode(true));
+
+//видалення елемента
+linkPara.remove();
+
+para.style.color = "white";
+para.style.backgroundColor = "black";
+para.style.padding = "10px";
+para.style.width = "250px";
+para.style.textAlign = "center";
+
+para.classList.add("formated");
+
+//створюємо годинник
+
+const clock = document.querySelector(".clock");
+const showClock = () => (clock.innerText = new Date().toLocaleTimeString());
+setInterval(showClock, 1000);
+
+$(click - me).click(() => $("img").toggle(3000));
+
+//будуємо калькулятор
+const display = document.querySelector(".display");
+
+document
+  .querySelectorAll(".digits button") //знаходимо усі кнопки з цифрами
+  .forEach((button) => button.addEventListener("click", digitPressed)); //на всі кнопки присвоюємо єдину функцію digitPressed
+
+function digitPressed(ev) {
+  display.value += ev.target.innerText;
 }
